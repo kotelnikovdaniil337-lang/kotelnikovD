24768e3d3b3397c040f52e0cce30f52329d5c425
daniil <248486230+kotelnikovdaniil337-lang@users.noreply.github.com>
2026-04-16 08:32:18 +0000

    Последовательный ввод массы предметов, помещаемых в автомобиль. Ввод оканчивается нулём

--- kotelnikovD.cpp
+++ kotelnikovD.cpp
@@ -8,4 +8,16 @@ int main()
     double maspred;
     cout << "Введите грузоподъёмность автомобиля (кг): ";
     cin >> gruzpod;
+    cout << "Введите массу каждого предмета (кг). Для завершения введите 0:";
+    while (true) {
+        cout << "Масса предмета: ";
+        cin >> maspred;
+        
+        if (maspred == 0) {
+            break;
+        }
+         obmas += maspred;
+        k++;
+    }
+    return 0;
 }
