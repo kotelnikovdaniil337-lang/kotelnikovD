503e7188d5241792c88f3745c22dcc762dd8c46e
daniil <248486230+kotelnikovdaniil337-lang@users.noreply.github.com>
2026-04-16 07:55:12 +0000

    Ввод максимальной грузоподъёмности автомобиля

--- kotelnikovD.cpp
+++ kotelnikovD.cpp
@@ -1,1 +1,11 @@
 #include <iostream>
+using namespace std;
+int main()
+{
+    double gruzpod;
+    double obmas = 0;
+    int k = 0;
+    double maspred;
+    cout << "Введите грузоподъёмность автомобиля (кг): ";
+    cin >> gruzpod;
+}
