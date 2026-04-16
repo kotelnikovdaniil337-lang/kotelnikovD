b0b12f0e2db6a37d58c4952d6239a51699891023
daniil <248486230+kotelnikovdaniil337-lang@users.noreply.github.com>
2026-04-16 08:58:43 +0000

    Последовательный ввод сопротивлений. Ввод оканчивается нулём.

--- kotelnikovD.cpp
+++ kotelnikovD.cpp
@@ -2,37 +2,22 @@
 using namespace std;
 int main()
 {
-    double gruzpod;
-    double obmas = 0;
+    double sopr;
+    double sumobr = 0;  
     int k = 0;
-    double maspred;
-    cout << "Введите грузоподъёмность автомобиля (кг): ";
-    cin >> gruzpod;
-    cout << "Введите массу каждого предмета (кг). Для завершения введите 0:";
+    cout << "Введите сопротивления элементов (Ом). Для завершения введите 0:";
     while (true) {
-        cout << "Масса предмета: ";
-        cin >> maspred;
-        
-        if (maspred == 0) {
+        cout << "Сопротивление: ";
+        cin >> sopr;
+        if (sopr == 0) {
             break;
         }
-         obmas += maspred;
+        if (sopr < 0) {
+            cout << "Сопротивление не может быть отрицательным";
+            continue;
+        }
+        sumobr += 1.0 / sopr;
         k++;
     }
-    cout << "Общая масса предметов: " << obmas << " кг";
-    cout << "Количество предметов: " << k << "\n";
-    
-    double процентЗагрузки = (obmas / gruzpod) * 100;
-    cout << "Процент загрузки: " << процентЗагрузки << "%\n";
-     if (obmas > gruzpod) {
-        cout << "ПЕРЕГРУЗ!"
-             << (obmas - gruzpod) << " кг.\n";
-    } else {
-        cout << "Перегруз отсутствует\n";
-    }
-    cout << "Нажмите Enter для завершения работы";
-    cin.ignore(); 
-    cin.get();   
-
     return 0;
 }
