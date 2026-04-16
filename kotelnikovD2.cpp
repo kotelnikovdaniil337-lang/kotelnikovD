d8845e53ef2f998724b01a688ff72aee3f45fd3e
daniil <248486230+kotelnikovdaniil337-lang@users.noreply.github.com>
2026-04-16 09:08:37 +0000

    Последовательный ввод сопротивлений. Ввод оканчивается нулём.

--- kotelnikovD2.cpp
+++ kotelnikovD2.cpp
@@ -2,7 +2,22 @@
 using namespace std;
 int main()
 {
-    
-    
+    double sopr;
+    double sumob = 0; 
+    int k = 0;
+    cout << "Введите сопротивления элементов (Ом).";
+    while (true) {
+        cout << "Сопротивление: ";
+        cin >> sopr;
+        if (sopr == 0) {
+            break;
+        }
+        if (sopr < 0) {
+            cout << "Сопротивление не может быть отрицательным! Попробуйте снова.\n";
+            continue;
+        }
+        sumob += 1.0 / sopr;
+        k++;
+    }
     return 0;
 }
